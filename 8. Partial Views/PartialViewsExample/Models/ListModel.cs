cae0b5bba13af9ae57fa80ae12cd72e86eb4ae72
﻿Roman Myshko <romamyshko12@gmail.com>
2025-06-10 18:56:32 +0300

    Partial Views are done.

---
+++ 8. Partial Views/PartialViewsExample/Models/ListModel.cs
@@ -0,0 +1,8 @@
+namespace PartialViewsExample.Models
+{
+    public class ListModel
+    {
+        public string ListTitle { get; set; } = "";
+        public List<string> ListItems { get; set; } = new List<string>();
+    }
+}

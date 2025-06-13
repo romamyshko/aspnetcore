9d05c46268e4ecb036f5fd94a207324ff5b4c03f
﻿Roman Myshko <romamyshko12@gmail.com>
2025-06-13 20:47:09 +0300

    Environments section is done.

---
+++ 11. Environments/EnvironmentsExample/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace EnvironmentsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public HomeController(IWebHostEnvironment webHostEnvironment)
+        {
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewBag.CurrentEnviornment = _webHostEnvironment.EnvironmentName;
+            return View();
+        }
+    }
+}

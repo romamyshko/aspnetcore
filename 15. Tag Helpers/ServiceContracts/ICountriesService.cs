b8d7b3c91b24d773024d9b6c5f4cbb549a4fbc37
﻿Roman Myshko <romamyshko12@gmail.com>
2025-06-18 13:05:28 +0300

    Added tag helpers.

---
+++ 15. Tag Helpers/ServiceContracts/ICountriesService.cs
@@ -0,0 +1,13 @@
+using ServiceContracts.DTO;
+
+namespace ServiceContracts
+{
+    public interface ICountriesService
+    {
+        CountryResponse AddCountry(CountryAddRequest? countryAddRequest);
+
+        List<CountryResponse> GetAllCountries();
+
+        CountryResponse? GetCountryByCountryID(Guid? countryID);
+    }
+}
